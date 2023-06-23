f8afe517b9c411113e1a35d79dfa60600af69986
Domenico <135279072+DevDomenico@users.noreply.github.com>
2023-06-23 19:54:42 +0200

    esercizioD2

---
+++ index.js
@@ -0,0 +1,113 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* Ecco unesempio semplificato dei datatype in JavaScript 
+1) Numeri (Numbers): Puoi mettere numeri nella scatola. Ad esempio, potresti mettere il numero 5 o il numero 10. 
+I numeri ti aiutano a fare calcoli o contare oggetti.
+
+2)Stringhe (Strings): Puoi mettere parole o frasi nella scatola. Per esempio, potresti mettere la parola "cane" o la frase "Mi piace il gelato". 
+Le stringhe sono utili quando vuoi lavorare con il testo.
+
+3)Booleani (Booleans): Puoi mettere un piccolo segnalibro nella scatola che può essere vero o falso. Immagina di avere un segnalibro verde per "vero" e un segnalibro rosso per "falso". 
+I booleani ti aiutano a prendere decisioni.
+
+4)Array: Puoi mettere una lista di cose nella scatola. Ad esempio, potresti mettere una lista dei tuoi giocattoli preferiti. 
+Puoi mettere i tuoi giocattoli preferiti uno accanto all'altro all'interno della scatola. 
+Puoi accedere a ciascun giocattolo in base alla sua posizione nella lista.
+
+5)Oggetti (Objects): Puoi mettere un gruppo di informazioni correlate nella scatola. Ad esempio, potresti mettere un oggetto chiamato "casa" nella scatola.
+ All'interno dell'oggetto "casa", puoi avere informazioni come il colore delle pareti, il numero di stanze e i mobili presenti. 
+Puoi accedere a ciascuna informazione dell'oggetto utilizzando il suo nome */
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* Immaginiamo che un oggetto in JavaScript sia come una valigia magica che può contenere molti oggetti diversi. 
+Ogni oggetto all'interno della valigia ha un nome, come "giocattolo" o "libro", e ha anche alcune informazioni associate ad esso.
+
+Ad esempio, se l'oggetto è una valigia magica chiamata "casa", all'interno potresti trovare oggetti come "letto", "tavolo" e "sedia". 
+Ogni oggetto ha delle caratteristiche specifiche. Ad esempio, il letto potrebbe avere una dimensione, il tavolo potrebbe avere una forma 
+e la sedia potrebbe essere di un certo colore.
+
+L'oggetto ci permette di raggruppare queste informazioni in un unico posto, in modo che sia più facile accedervi e utilizzarle. Possiamo immaginare 
+l'oggetto come un contenitore organizzato che ci aiuta a tenere tutto in ordine.
+
+Quando lavoriamo con gli oggetti in JavaScript, possiamo accedere alle loro proprietà utilizzando il nome dell'oggetto seguito da un punto e il nome
+ della proprietà. Ad esempio, se vogliamo conoscere il colore della sedia nella valigia "casa", possiamo scrivere "casa.sedia.colore" per ottenere questa 
+informazione.
+
+In sostanza, un oggetto in JavaScript è un modo per tenere insieme diverse informazioni correlate, come se fosse una valigia magica che può contenere 
+oggetti di diverso tipo. */
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+
+let numero1 = 12;
+let numero2 = 20;
+
+const somma = numero1 + numero2;
+
+console.log(somma);
+/* In questo caso, ho utilizzato let per dichiarare le variabili numero1 e numero2. Successivamente, ho utilizzato const per dichiarare 
+la variabile somma, in quanto il suo valore non cambierà.
+
+L'addizione viene eseguita utilizzando l'operatore +, e il risultato viene assegnato alla variabile somma. 
+ */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/*  utilizzando la parola chiave let per dichiarare una variabile chiamata "x". 
+Successivamente, utilizzando l'operatore di assegnazione = per assegnare il valore 12 alla variabile "x". */
+let xy= 12;
+
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+let name = "Domenico";
+
+
+/* utilizzando la parola chiave let per dichiarare una variabile chiamata "name". Successivamente, utilizzando 
+l'operatore di assegnazione = per assegnare il valore "Domenico" alla variabile "name */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+let x = 12;
+let risultato = 4 - x;
+
+console.log(risultato);
+
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+let name1 = "john";
+let name2 = "John";
+
+console.log(name1 !== name2); // Verifica che name1 sia diversa da name2
+
+console.log(name1.toLowerCase() === name2.toLowerCase()); // Verifico che la loro uguaglianza sia true quando entrambe vengono trasformate in lowercase
+
+/* SCRIVI QUI LA TUA RISPOSTA */
